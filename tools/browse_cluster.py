51f4b2823b1b79ad5f0e423977c8c2fcb3945f3c
Zach Riggle <zachriggle@gmail.com>
2010-12-15 00:04:06 -0500

    Added cluster browser

---
+++ tools/browse_cluster.py
@@ -0,0 +1,34 @@
+"""
+Browse search results by cluster.
+
+Usage:
+    python tools/browse_cluster.py 1 2 3 4
+
+Displays images from clusters 1,2,3,4 in the browser (on Mac OS X, or any system that supports the 'open' command).
+
+"""
+
+from lemur import *
+
+def browseCluster(which):
+    results = executeQuery("""
+    <DOC 1>
+    %s
+    </DOC>
+    """ % which)
+    
+    print results
+    
+    for i in results:
+        system("open http://s3.amazonaws.com/cse484-images/%s" % i)
+
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) == 1:
+        browseCluster(0)
+    
+    else:
+        for i in sys.argv[1:]:
+            browseCluster(i)
