a909b923093c6e3379cb4d6b996d1178d2f04c7d
Zach Riggle <zachriggle@gmail.com>
2010-12-16 08:56:40 -0500

    Added script to download and extract features

---
+++ get-features.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Download
+wget http://www.cse.msu.edu/~tongwei/cse484project.tar.gz
+
+# Extract
+tar xvzf cse484project.tar.gz cse484project/features/
+
+# Move
+mkdir features
+mv cse484project/features/* features
+
+# Remove directories. Don't rm -rf in case there's something else there
+rmdir cse484project/features
+rmdir cse484project
